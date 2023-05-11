49c368b6fb04441fad0bfe3e50bdb6cdc3396e4a
孟凡荣 <mfr01606587@alibaba-inc.com>
2023-05-11 17:21:56 +0800

    feat: 接入路由

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./views/Home";
+import About from "./views/About";
+
 function App() {
   return (
     <div className="App">
-      App
+      <div>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/about">About</Link>
+        </button>
+      </div>
+
+      <header className="App-header">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </header>
     </div>
   );
 }
